cd26bc65d9922f0d8822dc95192d2c9cc83b9d4b
Ben Doggett <bdoggett@gmail.com>
2025-01-25 10:36:51 -0500

    Complete exercise. Be careful, deleting posts will remove posts permanently, the posts.js file in app/api/posts.js fill be altered by the delete request.

--- app/page.js
+++ app/page.js
@@ -1,37 +1,44 @@
-'use client';
-import styles from './page.module.css';
-import Link from 'next/link';
-import { useSelector } from 'react-redux';
+"use client";
+import styles from "./page.module.css";
+import Link from "next/link";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchPosts } from "./store/slices/posts";
+import { useEffect } from "react";
 
 export default function Home() {
-	const posts = useSelector((state) => state.posts.posts);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
 
-	const renderPosts = () => {
-		if (posts.length > 0) {
-			return posts.map((post) => {
-				return (
-					<li className='list-group-item' key={post.id}>
-						<Link href={`/posts/${post.id}`}>{post.title}</Link>
-					</li>
-				);
-			});
-		} else {
-			return <div>No posts to show</div>;
-		}
-	};
+  const posts = useSelector((state) => state.posts.posts);
 
-	return (
-		<main className={styles.main}>
-			<div>
-				<div className='text-xs-right'>
-					<Link className='btn btn-primary' href='/posts/new'>
-						Add a Post
-					</Link>
-				</div>
-				<br></br>
-				<h3>Posts</h3>
-				<ul className='list-group'>{renderPosts()}</ul>
-			</div>
-		</main>
-	);
+  const renderPosts = () => {
+    if (posts.length > 0) {
+      return posts.map((post) => {
+        return (
+          <li className="list-group-item" key={post._id}>
+            <Link href={`/posts/${post._id}`}>{post.title}</Link>
+          </li>
+        );
+      });
+    } else {
+      return <div>No posts to show</div>;
+    }
+  };
+
+  return (
+    <main className={styles.main}>
+      <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" href="/posts/new">
+            Add a Post
+          </Link>
+        </div>
+        <br></br>
+        <h3>Posts</h3>
+        <ul className="list-group">{renderPosts()}</ul>
+      </div>
+    </main>
+  );
 }
---
+++ app/posts/[id]/page.jsx
@@ -0,0 +1,55 @@
+"use client";
+import Link from "next/link";
+import { useEffect } from "react";
+import { useParams, useRouter } from "next/navigation";
+import { useSelector, useDispatch } from "react-redux";
+import { deletePost, fetchPost } from "../../store/slices/posts";
+
+const PostsShow = () => {
+  const { id } = useParams();
+
+  const router = useRouter();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPost(id));
+  }, [dispatch, id]);
+
+  const post = useSelector((state) => state.posts.post);
+
+  const onDeleteClick = () => {
+    dispatch(deletePost(id));
+    router.push("/");
+  };
+
+  const renderCategories = () => {
+    return post?.categories?.map?.((category) => {
+      return (
+        <span className="label label-primary" key={category}>
+          {category}
+        </span>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <Link href="/">Back To Index</Link>
+      <br></br>
+      <br></br>
+      <button className="btn btn-danger pull-xs-right" onClick={onDeleteClick}>
+        Delete Post
+      </button>
+      <br></br>
+      <br></br>
+      <h3>{post?.title}</h3>
+      <h6>
+        <strong>Categories:</strong> {renderCategories()}
+      </h6>
+      <p>{post?.content}</p>
+    </div>
+  );
+};
+
+export default PostsShow;
